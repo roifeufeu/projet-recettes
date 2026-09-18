7b631e921919dc0aab4ee0d06e7e5d646bbf29c2
roifeufeu <jorickbolivar@gmail.com>
2026-09-18 14:41:00 -0400

    design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,20 @@ function Home() {
       <Header />
 
       <main>
-        <h1>Trouvez la recette qu'il vous faut</h1>
+        <section className="hero">
+          <h1>Trouvez la recette qu'il vous faut</h1>
 
-        <p>
-          Entrez le nom d'un plat pour découvrir ses ingrédients
-          et les quantités nécessaires.
-        </p>
+          <p>
+            Entrez le nom d'un plat pour découvrir ses ingrédients
+            et les quantités nécessaires.
+          </p>
 
-        <SearchBar />
+          <SearchBar />
+        </section>
+
+        <section className="results">
+          {/* Les résultats seront affichés ici */}
+        </section>
       </main>
     </>
   )
