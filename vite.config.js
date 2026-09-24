cd65486093affdb86cd3e35d4920850233724315
roifeufeu <102964284+roifeufeu@users.noreply.github.com>
2026-09-24 10:35:04 -0400

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Projet Recettes",
+        short_name: "Recettes",
+        description:
+          "Recherchez un plat et obtenez ses ingrédients et ses quantités.",
+
+        theme_color: "#ea580c",
+        background_color: "#fff7ed",
+
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
